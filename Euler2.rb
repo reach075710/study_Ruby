52f015a681b2feacaca65b4dbfbd9da9a3bbb284
reach075710 <peach07571@gmail.com>
2019-01-30 22:36:51 +0900

    Answer for Euler2

--- Euler2.rb
+++ Euler2.rb
@@ -1,17 +1,20 @@
 i = 1 #n番目
 j = 0 #n-1番目
 k = 0 #n+2番目
-SUM_even_vulued_term = 0
+l = 0
 
 #main
-while i <= 4.0 * ( 10 ** 6 ) do
+loop do
     k = j
     j = i
     i = j + k
-    
+    if i > 4.0 * ( 10 ** 6 ) then
+        break
+    end
+
     if i % 2 == 0 then
-        SUM_even_vulued_term += i
+            l += i
     end
 end
 
-puts i
+puts l
