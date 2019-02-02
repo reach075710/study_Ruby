c1be658da322414d5cff55fb5508dde31d3156bc
reach075710 <peach07571@gmail.com>
2019-02-03 00:06:28 +0900

    change fro

--- Euler4.rb
+++ Euler4.rb
@@ -9,7 +9,9 @@ answer = 0
 
 #MAIN
 
-# for i in 100..999
-#     for j in 100..999
-#         answer = i * j
-
+for i in 100..999
+    for j in 100..999
+        answer = i * j
+# puts i * j
+# check_number_length = (i * j).to_s
+# p check_number_length
