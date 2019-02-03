bbd61f2b596fbb1e3eca2c6c4d3ebd64b611bc4e
reach075710 <peach07571@gmail.com>
2019-02-03 23:33:43 +0900

    End test [length method]

--- Euler4.rb
+++ Euler4.rb
@@ -5,6 +5,8 @@
 #SET
 i = 100
 j = 100
+k = 0
+
 answer = 0
 
 #MAIN
@@ -12,6 +14,18 @@ answer = 0
 for i in 100..999
     for j in 100..999
         answer = i * j
+        answer_float_to_str = answer.to_s
+        if answer_float_to_str.length % 2 == 0 then
+            head_leng = answer_float_to_str.length / 2 
+            for k in 1..head_leng
+        
+        
+            
+        end
+
+    end
+end
+
 # puts i * j
 # check_number_length = (i * j).to_s
 # p check_number_length
