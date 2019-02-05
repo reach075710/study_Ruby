71381731ea7a4b9c5046cfa83f8a9b8efe558a6f
reach075710 <peach07571@gmail.com>
2019-02-05 23:47:35 +0900

    tochu

--- Euler5.rb
+++ Euler5.rb
@@ -3,6 +3,26 @@
 #nまでの数字で１種類の素因数しか持たない数を全て掛け合わせれば良い
 
 i = 0
+j = 0
+k = 0
+prime_factor_arr = Array.new
 
+#MAIN
 
+for i in 1..n
+    j = 2   #jは2以上
+    loop do
+        if i % j == 0 then
+            prime_factor_arr[k] = j
+            i = i / j
 
+            if i % j != 0 then
+                break
+            end
+          
+            k += 1
+        end
+        j += 1
+    end
+
+end
