575c2ded2b37ced0d54d73bca5c75f2d90a8d33a
reach075710 <peach07571@gmail.com>
2019-02-10 16:19:54 +0900

    fix Euler5

--- Euler5.rb
+++ Euler5.rb
@@ -5,10 +5,12 @@
 i = 0
 j = 0
 k = 0
+answer = 1
 prime_factor_arr = Array.new
+powered_prime_number_arr = Array.new
 
 #MAIN
-n = 10
+n = 20
 for i in 2..n
    for j in 2..i
         if i % j == 0 then
@@ -16,9 +18,20 @@ for i in 2..n
                 break
             else
                 prime_factor_arr[k] = i
+                powered_prime_number_arr[k] = i
                 k += 1
             end
         end
     end
 end
-puts prime_factor_arr
+
+for i in 0..k-1
+    loop do
+        if n <= powered_prime_number_arr[i] * prime_factor_arr[i] then
+            break
+        end
+        powered_prime_number_arr[i] = powered_prime_number_arr[i] * prime_factor_arr[i]
+    end
+    answer = answer * powered_prime_number_arr[i]
+end
+puts answer
