277c8e58e88415dc5a9d857b2361ada2b9666c1a
reach075710 <peach07571@gmail.com>
2019-09-25 22:46:34 +0900

    start Euler8

--- ProjectEurer/Euler8.rb
+++ ProjectEurer/Euler8.rb
@@ -1,13 +1,25 @@
 # n桁の数字の中から連続するm個の数を掛け合わせたもので最も大きい数を求める
 
 #SET
-n = 123456
-m = 5
+input_number = "12345012345601234567"
+min_length = 6
 
 i = 0
 j = 0
 k = 0
 
 progression_arr = Array.new
+arr_number = 0
 
 #MAIN
+progression_arr = input_number.split("0")
+arr_number = progression_arr.length
+
+for i in 0..arr_number
+    if progression_arr[i].length >= min_length then
+        
+
+
+    end
+
+end
