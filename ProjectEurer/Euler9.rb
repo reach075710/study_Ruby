af75defc70f954b1cad10eaecac41e2105a49ca5
reach075710 <peach07571@gmail.com>
2019-09-27 23:14:51 +0900

    start problem 9

---
+++ ProjectEurer/Euler9.rb
@@ -0,0 +1,24 @@
+#ピタゴラス数を求める
+
+#set
+a = 0
+b = 0
+c = 0
+sum_abc = 1000
+max_a = sum_abc / 3 - 1
+min_c = sum_abc / 3 + 1
+
+#main
+for a in 1..max_a
+    
+    c = Math.sqrt( a ** 2 + b ** 2 )
+    if c < b then
+        if c =~ /^[0-9]+$/ then
+            if a + b + c = sum_abc
+                break
+            end
+        end
+    end
+end
+
+p a * b * c
