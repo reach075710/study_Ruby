399113ba47510fcde3b8fb7b1c6c8ae95dbf4328
reach075710 <peach07571@gmail.com>
2019-10-12 00:02:58 +0900

    Accept ABC_085_C by full Search

--- AtCoder/ABC_085_C.rb
+++ AtCoder/ABC_085_C.rb
@@ -4,26 +4,17 @@ yukichi = 0
 higuchi = 0
 noguchi = 0
 
-if y >= 10000 then
-    yukichi = y / 10000
-    y -= yukichi * 10000
+(0 .. n ).each do |yukichi|
+    (0 .. n - yukichi).each do |higuchi|
+        noguchi = n - yukichi - higuchi
+        if noguchi < 0 then
+            break
+        end
+        if y == yukichi * 10000 + higuchi * 5000 + noguchi * 1000 then
+            print ("#{yukichi} #{higuchi} #{noguchi}\n")
+            exit
+        end
+    end
 end
 
-if y >= 5000 then
-    higuchi = y / 5000
-    y -= higuchi * 5000
-end
-
-if y >= 1000
-    noguchi = y / 1000
-    y -= noguchi * 1000
-end
-
-sum = yukichi + higuchi + noguchi
-if y != 0 || sum > n then
-    yukichi = -1
-    higuchi = -1
-    noguchi = -1
-end
-
-print ("#{yukichi} #{higuchi} #{noguchi}\n")
+print ("#{-1} #{-1} #{-1}\n")
