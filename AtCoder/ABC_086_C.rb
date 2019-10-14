c5b8f66b4dd55f1a4982c9fa77e0e4768bb32373
reach075710 <peach07571@gmail.com>
2019-10-14 14:29:54 +0900

    Accept ABC_086_C

--- AtCoder/ABC_086_C.rb
+++ AtCoder/ABC_086_C.rb
@@ -3,13 +3,27 @@ n = gets.chomp.to_i
 t = Array.new
 x = Array.new
 y = Array.new
-now = Array.new
+now = Array.new(2,0)
+answer = 'Yes'
+t[0] = 0
+x[0] = 0
+y[0] = 0
 (0 .. n - 1).each do |i|
      plan_arr = gets.chomp.split(" ").map(&:to_i)
-     t[i] = plan_arr[0] 
-     x[i] = plan_arr[1]
-     y[i] = plan_arr[2]
-     now[i] = ([x[i],y[i]])
+     t[i + 1] = plan_arr[0] 
+     x[i + 1] = plan_arr[1]
+     y[i + 1] = plan_arr[2]
+     now[i + 1] = ([x[i + 1],y[i + 1]])
 end
 
 #main
+(0 .. n - 1).each do |j|
+    distance = (x[j + 1] - x[j]).abs + (y[j + 1] - y[j]).abs
+    time = (t[j + 1] - t[j]).abs
+    if distance > time || (time - distance) % 2 == 1 then
+        answer ='No'
+        break
+    end
+end
+
+print ("#{answer}\n")
