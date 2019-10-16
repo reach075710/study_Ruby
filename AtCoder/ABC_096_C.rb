a97a320910ec0b6948f81ae1cf0cbb7b1b6372c4
reach075710 <peach07571@gmail.com>
2019-10-17 00:22:24 +0900

    Accept ABC_096_C

--- AtCoder/ABC_096_C.rb
+++ AtCoder/ABC_096_C.rb
@@ -14,17 +14,23 @@ end
 #main
 (0 .. h - 1).each do |i|
     (0 .. w - 1).each do |j|
+        # p squares[i][j]
         if squares[i][j] == "#" then
             chk_flag = 0
             (i - 1 .. i + 1).each do |x|
                 (j - 1 .. j + 1).each do |y|
-                    if ( x == i || y == j ) && x != y && x >= 0 && x < h && y >= 0 && y < w then
-                        if squares[x][y] == "#" then
-                            chk_flag += 1
+                    if ( x == i || y == j ) && ( x >= 0 && x < h ) && ( y >= 0 && y < w ) then
+                        if x == i && y == j then
+                            next
+                        else
+                            if squares[x][y] == "#" then
+                                chk_flag += 1
+                            end
                         end
                     end
                 end
             end
+            
             if chk_flag == 0 then
                 answer = "No"
                 print ("#{answer}\n")
@@ -35,6 +41,3 @@ end
 end
 
 print ("#{answer}\n")
-# (0 .. h - 1).each do |i|
-# print ("#{squares[i].join}\n")
-# end
