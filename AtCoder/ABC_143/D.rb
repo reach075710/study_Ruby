ff6a447dfe0e74809c6e10a08d17017ce53be07d
reach075710 <peach07571@gmail.com>
2019-10-22 22:07:05 +0900

    refactaring ABC_143 D

--- AtCoder/ABC_143/D.rb
+++ AtCoder/ABC_143/D.rb
@@ -5,17 +5,17 @@ count = 0
 
 #main
 0.upto(n - 1) do |i|
-    j = i + 1
-    k = n - 1
+	j = i + 1
+	k = n - 1
 
-    while j < k do
-        if s[k] > s[i] - s[j] then
-            count += (k - j)
-            j += 1
-        else
-            k -= 1
-        end
-    end
+	while j < k do
+		if s[k] > s[i] - s[j] then
+			count += (k - j)
+			j += 1
+		else
+			k -= 1
+		end
+	end
 end
 
 print ("#{count}\n")
