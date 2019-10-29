1dc91b4b06371899dad95240bb23eff819a97cf5
reach075710 <peach07571@gmail.com>
2019-10-29 23:19:01 +0900

    pending ARC_061_C

--- AtCoder/past_problem/ARC_061_C.rb
+++ AtCoder/past_problem/ARC_061_C.rb
@@ -1,12 +1,19 @@
 #set
-s = gets.chomp.split("").map(&:to_i)
-s_length = s.join.length
+# s = gets.chomp.split("")
+# s_length = s.join.length
+# sum = 0
 
 #main
-s.repeated_permutation(s_length - 1) do |bits|
-  p bits
-
-
-
+s = gets.chomp
+t = ""
+ans = 0
+["", "+"].repeated_permutation(s.length) do |plus|
+  (s.length).times do |i|
+    t << plus[i]
+    t << s[i]
+  end
+  p t
+  ans += t.split("+").map(&:to_i).inject(:+)
+  t = ""
 end
-
+p ans / 2
