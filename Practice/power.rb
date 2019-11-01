496982698d3c55307f20539ee21f25b0beb3077f
reach075710 <peach07571@gmail.com>
2019-11-01 22:05:01 +0900

    try to power by binary method

--- Practice/power.rb
+++ Practice/power.rb
@@ -2,12 +2,20 @@
 # a の b乗を求める
 a,b = gets.chomp.split(" ").map(&:to_i)
 
-
 #main
-def power (a, b)
+def power(a, b)
   return 1 if b == 0
 
-  a *= power(a, b - 1)
+  answer = power(a, b / 2)
+  answer *= answer
+  answer *= a if b % 2 == 1
+  return answer
 end
+
+# def power (a, b)
+#   return 1 if b == 0
+
+#   a *= power(a, b - 1)
+# end
 
 p power(a, b)
