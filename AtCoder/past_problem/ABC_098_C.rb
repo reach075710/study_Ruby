15a0798889149fcd2b4002f67383275165f5a116
reach075710 <peach07571@gmail.com>
2019-11-12 22:49:28 +0900

    fulls search

--- AtCoder/past_problem/ABC_098_C.rb
+++ AtCoder/past_problem/ABC_098_C.rb
@@ -7,31 +7,43 @@ min_point = 0
 
 
 #main
-if n == 2 then
+# if n == 2 then
+#   0.upto(n - 1) do |i|
+#     count = 0
+#     if n == 2 && i == 0 then
+#       if s[1] == 'E' then
+#         count += 1
+#       end
+#     else
+#       if s[0] == 'W' then
+#         count += 1
+#       end
+#     end
+#     min_count = count if min_count > count
+#   end
+# else  
   0.upto(n - 1) do |i|
     count = 0
-    if n == 2 && i == 0 then
-      if s[1] == 'E' then
-        count += 1
+    if i == 0 then
+      (i + 1).upto(n - 1) do |k|
+        count += 1 if s[k] == 'E'
+      end
+    elsif i == n - 1 then
+      0.upto(i - 1) do |j|
+        count += 1 if s[j] == 'W'
       end
     else
-      if s[0] == 'W' then
-        count += 1
+      0.upto(i - 1) do |j|
+        count += 1 if s[j] == 'W'
+      end
+      (i + 1).upto(n - 1) do |k|
+        count += 1 if s[k] == 'E'
       end
     end
+    # print ("i=#{i}\n")
+    # print ("count=#{count}\n")
+    min_count = count if min_count >= count
   end
-  min_count = count if min_count > count
-else  
-  1.upto(n - 2) do |i|
-    count = 0
-    0.upto(i - 1) do |j|
-      count += 1 if s[j] == 'W'
-    end
-    (i + 1).upto(n - 1) do |k|
-      count += 1 if s[k] == 'E'
-    end
-  end
-end
+# end
 
 p min_count
-    
