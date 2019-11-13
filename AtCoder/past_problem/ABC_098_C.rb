43b40b71bde33f5e51e429c6c370c4800b600075
reach075710 <peach07571@gmail.com>
2019-11-14 00:06:51 +0900

    Try to Cumulative sum, Accept ABC_098_C

--- AtCoder/past_problem/ABC_098_C.rb
+++ AtCoder/past_problem/ABC_098_C.rb
@@ -2,48 +2,25 @@
 n = gets.chomp.to_i
 s = gets.chomp.split("")
 count = 0
+count_w = s.count('W')
+count_e = n - count_w
+count_ary = []
 min_count = n
 min_point = 0
 
 
 #main
-# if n == 2 then
-#   0.upto(n - 1) do |i|
-#     count = 0
-#     if n == 2 && i == 0 then
-#       if s[1] == 'E' then
-#         count += 1
-#       end
-#     else
-#       if s[0] == 'W' then
-#         count += 1
-#       end
-#     end
-#     min_count = count if min_count > count
-#   end
-# else  
-  0.upto(n - 1) do |i|
-    count = 0
-    if i == 0 then
-      (i + 1).upto(n - 1) do |k|
-        count += 1 if s[k] == 'E'
-      end
-    elsif i == n - 1 then
-      0.upto(i - 1) do |j|
-        count += 1 if s[j] == 'W'
-      end
-    else
-      0.upto(i - 1) do |j|
-        count += 1 if s[j] == 'W'
-      end
-      (i + 1).upto(n - 1) do |k|
-        count += 1 if s[k] == 'E'
-      end
-    end
-    # print ("i=#{i}\n")
-    # print ("count=#{count}\n")
-    min_count = count if min_count >= count
+# Cumulative sum
+0.upto(n - 1) do |i|
+  if i == 0 then
+    count_ary[i] = n - 1 - (count_w - 1) if s[i] == 'W'
+    count_ary[i] = n - 1 - count_w if s[i] == 'E'
+  else
+    count += 1 if s[i - 1] == 'W'
+    count_ary[i] = count + ((n - i - 1)  - (count_w - count - 1)) if s[i] == 'W'
+    count_ary[i] = count + ((n - i - 1)  - (count_w - count)) if s[i] == 'E'
   end
-# end
+end
+count_ary.sort!
 
-p min_count
+print ("#{count_ary[0]}\n")
