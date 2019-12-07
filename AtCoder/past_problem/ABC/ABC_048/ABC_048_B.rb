e782ce056c0ece97d7cbb1a8cc4099869862b36b
reach075710 <peach07571@gmail.com>
2019-12-07 23:38:33 +0900

    Accept ABC_048_A

--- AtCoder/past_problem/ABC/ABC_048/ABC_048_B.rb
+++ AtCoder/past_problem/ABC/ABC_048/ABC_048_B.rb
@@ -3,12 +3,6 @@ a,b,x = gets.chomp.split(" ").map(&:to_i)
 count = 0
 
 #main
-<<<<<<< HEAD
-a.upto(b) do |i|
-  count += 1 if i % x == 0
-end
-
-=======
 max_count = b / x
 if a >= 0 then
   min_count = (a - 1) / x
@@ -17,5 +11,4 @@ else
 end
 
 count = max_count - min_count
->>>>>>> ABC_048
 print ("#{count}\n")
