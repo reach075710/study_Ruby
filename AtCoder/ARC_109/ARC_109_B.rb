34406f2d9b2a61ac865d860b53519ba71a9405db
reach075710 <peach07571@gmail.com>
2020-11-30 23:34:08 +0900

    Fix ARC_109_B

--- AtCoder/ARC_109/ARC_109_B.rb
+++ AtCoder/ARC_109/ARC_109_B.rb
@@ -1,22 +1,32 @@
 # Set
 n = gets.chomp.to_i
 not_choice = 0
-answer = n
+answer = n + 1
 
 # main
 min = 1
 max = n
+
+if n == 1 || n == 2 then
+  print ("1\n")
+  exit
+end
+
 loop do
-  sum = (min + max) * (max - min + 1) / 2 
-  k = max / 2
+  k = (max + min) / 2
+  if max == (min + 1) then
+    not_choice = k
+    break
+  end
+  sum = (1 + k) * k / 2 
   max = k if sum > n + 1
   min = k if sum <= n + 1
-  print ("sum=#{sum}\n")
-  print ("max=#{max}\n")
-  print ("min=#{min}\n")
-  print ("k=#{k}\n")
-  sleep 1
-  
+  # print ("max=#{max}\n")
+  # print ("min=#{min}\n")
+  # print ("k=#{k}\n")
+  # print ("sum=#{sum}\n")
+  # sleep 1
+
 end
 answer -= not_choice
 
