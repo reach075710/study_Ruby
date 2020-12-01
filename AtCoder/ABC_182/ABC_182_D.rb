419070f85c5d335dc1d837401e046c47eccf538c
reach075710 <peach07571@gmail.com>
2020-12-01 23:22:59 +0900

    Fix ABC_182_D

--- AtCoder/ABC_182/ABC_182_D.rb
+++ AtCoder/ABC_182/ABC_182_D.rb
@@ -1,15 +1,19 @@
 # set
 n = gets.chomp.to_i
 a_arr = gets.chomp.split(" ").map(&:to_i)
+move_sum = 0
+move_max = 0
 now_where = 0
-max = 0
+max_where = 0
 
+# Main
 0.upto(n - 1) do |i|
-  0.upto(i) do |j|
-    now_where += a_arr[j]
-    max = now_where if max <= now_where
-  end
+  move_sum += a_arr[i]
+  move_max = move_sum if move_max <= move_sum
+
+  max_where = now_where + move_max if max_where <= now_where + move_max
+  now_where += move_sum
 end
 
 
-print ("#{max}\n")
+print ("#{max_where}\n")
